85a9ad7c7ddf79a64a17f246bab942d5004f70fb
MatthewGlenn <matthew.mark.moore@gmail.com>
2018-01-27 09:39:10 -0500

    Initial commit

---
+++ Assets/Scripts/ResponseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResponseController : MonoBehaviour {
+
+	public GameObject option1;
+	public GameObject option2;
+	public GameObject option3;
+
+	// Use this for initialization
+	void Awake () {
+		option2.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}

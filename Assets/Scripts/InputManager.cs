fc67b6ed9ec87a42ed90808dbee67d4322a0c54e
justinaray <justinaray828@gmail.com>
2018-01-27 11:12:11 -0500

    Syntax Fixes
    Syntax fixes

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject pauseMenu;
+
+	void Start(){
+		if (!pauseMenu) 
+			Debug.Log ("No pause menu attached");
+
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown ("Cancel")) {
+
+		}
 	}
 }

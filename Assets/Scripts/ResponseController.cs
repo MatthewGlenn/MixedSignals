a7d9c63e02f170497a531d2e58669081c720042d
justinaray <justinaray828@gmail.com>
2018-01-27 11:28:07 -0500

    Scene update with UI and Response Controller fleshed out more

--- Assets/Scripts/ResponseController.cs
+++ Assets/Scripts/ResponseController.cs
@@ -9,14 +9,26 @@ public class ResponseController : MonoBehaviour {
 	public GameObject option2;
 	public GameObject option3;
 
+	private Button option1Button;
+	private Button option2Button;
+	private Button option3Button;
+
 	// Use this for initialization
-	void Awake () {
-		option2.GetComponent<Button> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start () {
+		FindButtons ();
+		option2Button.Select ();
 	}
 
+	void FindButtons(){
+		option1Button = option1.GetComponent<Button> ();
+		option2Button = option2.GetComponent<Button> ();
+		option3Button = option3.GetComponent<Button> ();
+
+		if (!option1Button)
+			Debug.LogError ("Option1 Button not assigned to Response Controller");
+		if (!option2Button)
+			Debug.LogError ("Option2 Button not assigned to Response Controller");
+		if (!option3Button)
+			Debug.LogError ("Option3 Button not assigned to Response Controller");
+	}
 }
